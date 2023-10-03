dfbc7aa7661971c8b1d0c10d9c28f1c447d5388c
serjou <serhii.nikitin@remme.io>
2023-10-03 16:59:28 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,13 @@ import * as telegramBot from './modules/telegram-bot';
 import * as reminder from './modules/reminder';
 import { initizlizeDataSource } from './database/data-source';
 
+import * as dotenv from 'dotenv';
+
 const start = async () => {
+    dotenv.config({
+        path: './.env'
+    })
+
     await initizlizeDataSource()
 
     await telegramBot.initialize();
