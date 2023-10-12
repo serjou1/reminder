fe0a36163d25d5acc59105fb175de3e7a797a3bf
serjou <serhii.nikitin@remme.io>
2023-10-12 14:02:16 +0300

    changes

---
+++ src/modules/telegram-bot/constants/button-commands.ts
@@ -0,0 +1,2 @@
+export const ADD_THOUGHT_BT = '📝 Add thought';
+export const SETTINGS_BT = '⚙️ Settings';
--- src/modules/telegram-bot/handlers/text.ts
+++ src/modules/telegram-bot/handlers/text.ts
@@ -6,13 +6,16 @@ const textHandlers: {
     [state: string]: (user: BotUser, ctx: ContextType) => Promise<void>
 } = {};
 
+const buttonHandlers: {
+    [buttonText: string]: (ctx: ContextType) => Promise<void>
+} = {};
+
 export const textHandler = async (ctx: ContextType) => {
-    const user = await getUserFromContext(ctx);
-    
-    const handler = textHandlers[user.state];
-    if (handler) {
-        await handler(user, ctx);
+    if (await handleButtonPress(ctx)) {
+        return;
     }
+
+    await handleText(ctx);
 };
 
 export const setTextHandler = (
@@ -20,4 +23,30 @@ export const setTextHandler = (
     handler: (user: BotUser, ctx: ContextType) => Promise<void>
 ) => {
     textHandlers[state] = handler
+};
+
+export const setButtonHandler = (
+    buttonText: string,
+    handler: (ctx: ContextType) => Promise<void>
+) => {
+    buttonHandlers[buttonText] = handler;
+};
+
+const handleButtonPress = async (ctx: ContextType): Promise<boolean> => {
+    const handler = buttonHandlers[ctx.message.text];
+    if (handler) {
+        await handler(ctx);
+        return true;
+    }
+
+    return false;
+};
+
+const handleText = async (ctx: ContextType) => {
+    const user = await getUserFromContext(ctx);
+
+    const handler = textHandlers[user.state];
+    if (handler) {
+        await handler(user, ctx);
+    }
 };
