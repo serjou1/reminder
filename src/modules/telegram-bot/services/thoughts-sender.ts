03ee86314d37008bb8af75053188cea3c6b00c57
serjou <serhii.nikitin@remme.io>
2023-11-21 11:27:31 +0200

    fix user blocked bot

--- src/modules/telegram-bot/services/thoughts-sender.ts
+++ src/modules/telegram-bot/services/thoughts-sender.ts
@@ -1,8 +1,18 @@
+import { log } from "console";
+import { TelegramError } from "telegraf";
 import { bot } from "..";
 import { BotUser } from "../../../database/entities/bot-user";
 import { DefaultThought } from "../../../database/entities/default-thought";
 import { Thought } from "../../../database/entities/thought";
 
 export const sendThought = async (thought: Thought | DefaultThought, user: BotUser) => {
-    await bot.telegram.sendMessage(user.userId, thought.text);
+    try {
+        await bot.telegram.sendMessage(user.userId, thought.text);
+    } catch (e) {
+        if (e instanceof TelegramError && e.code == 403) {
+            log(`User ${user.userId} has blocked bot`);
+        } else {
+            throw e;
+        }
+    }
 };
